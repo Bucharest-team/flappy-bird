42296d5e79b25ca465b272c39dda9145fc5c5a44
Denis Shevchenko <DAlShevchenko@sberbank.ru>
2021-05-16 14:16:35 +0300

    Remove api folder and rewrite reducer

--- src/__data__/action-types.ts
+++ src/__data__/action-types.ts
@@ -1,3 +1,5 @@
 export const PROFILE_FETCH = 'profile/fetch';
 
 export const AUTH_LOGOUT_FETCH = 'auth/logout';
+export const AUTH_SIGNIN = 'auth/signin';
+export const AUTH_SIGNUP = 'auth/signup';
--- src/__data__/slices/user.ts
+++ src/__data__/slices/user.ts
@@ -1,6 +1,6 @@
 import { createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit';
 
-import { AUTH_LOGOUT_FETCH } from '../action-types';
+import { AUTH_LOGOUT_FETCH, AUTH_SIGNIN, AUTH_SIGNUP } from '../action-types';
 import { UserApi } from '../../api/user-api';
 import axios from '../axios';
 
@@ -9,12 +9,39 @@ type SliceState = {
     isLogin: boolean;
 };
 
+export type Registration = {
+    first_name: string;
+    second_name: string;
+    login: string;
+    email: string;
+    password: string;
+    passwordTwo?: string;
+    phone: string;
+};
+
+export type Login = {
+    login: string;
+    password: string;
+};
+
 const initialState: SliceState = { userID: null, isLogin: false };
 
 const LOGOUT_URL = '/auth/logout';
+const LOGIN_URL = '/auth/signin';
+const REGISTER_URL = '/auth/signup';
 
 export const logout = createAsyncThunk(AUTH_LOGOUT_FETCH, async () => {
     const { data } = await axios.post(LOGOUT_URL);
+    return data;
+});
+
+export const auth = createAsyncThunk(AUTH_SIGNIN, async (payload: Login) => {
+    const { data } = await axios.post(LOGIN_URL, payload);
+    return data;
+});
+
+export const register = createAsyncThunk(AUTH_SIGNUP, async (payload: Login) => {
+    const { data } = await axios.post(REGISTER_URL, payload);
     return data;
 });
 
@@ -34,32 +61,26 @@ const user = createSlice({
             localStorage.removeItem('login');
             state.isLogin = false;
         });
+        builder.addCase(auth.fulfilled, (state) => {
+            localStorage.setItem('login', 'true');
+            state.isLogin = true;
+        });
+        builder.addCase(auth.rejected, () => {
+            alert('Неверный логин или пароль!');
+        });
+        builder.addCase(register.fulfilled, (state) => {
+            localStorage.setItem('login', 'true');
+            state.isLogin = true;
+        });
+        builder.addCase(register.rejected, () => {
+            alert('Произошла ошибка!');
+        });
     }
 });
 export const { setUser, setLogin } = user.actions;
 
 export const checkLogin = () => (dispatch: any) => {
     dispatch(setLogin(Boolean(localStorage.getItem('login'))));
-};
-
-export const auth = (payload: any) => async (dispatch: any) => {
-    try {
-        await UserApi.login(payload);
-        localStorage.setItem('login', 'true');
-        dispatch(setLogin(true));
-    } catch (e) {
-        alert('Неверный логин или пароль!');
-    }
-};
-
-export const register = (payload: any) => async (dispatch: any) => {
-    try {
-        await UserApi.register(payload);
-        localStorage.setItem('login', 'true');
-        dispatch(setLogin(true));
-    } catch (e) {
-        alert('Неверный логин или пароль!');
-    }
 };
 
 export default user.reducer;
