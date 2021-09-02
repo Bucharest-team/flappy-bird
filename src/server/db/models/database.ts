36fd1f499bd14f5da5358da65f90ed2ef635fd6f
Sveta Grishaeva <sveta.grishaeva.2013@mail.ru>
2021-09-03 00:13:11 +0700

    deploy. yandex cloud

--- src/server/db/models/database.ts
+++ src/server/db/models/database.ts
@@ -1,7 +1,7 @@
 import { Sequelize } from 'sequelize';
 
 export const sequelize = new Sequelize('flappy-db', 'postgres', 'newPassword', {
-    host: 'localhost',
+    host: 'postgres',
     port: 5432,
     dialect: 'postgres'
 });
